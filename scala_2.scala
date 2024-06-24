b2ff594de5a429ef94768c9948033e0be405e0b0
CharithaGamlatha <140165762+CharithaGamlatha@users.noreply.github.com>
2024-06-25 00:24:00 +0530

    Update scala_2.scala

--- scala_2.scala
+++ scala_2.scala
@@ -24,6 +24,48 @@ object HelloWorld {
         println("x++ and ++x and --x and x-- is do not work in scala")
         println("println (- 2 * ( g - k ) +c); g is do not define variable and  '–' is not a '-' ")
 
+        object HelloWorld {
+    def main(args: Array[String]){
+        var a: Int = 2
+        var b: Int = 3
+        var c: Int = 4
+        var d: Int = 5
+        var k: Float = 4.3f
+        
+        println("a= " + a)
+        println("b= "+ b)
+        println("c= "+c)
+        println("d= "+d)
+        println("k= "+ k)
+        
+        // and evaluate the expressions
+        
+        // println( - -b * a + c *d - -);
+        // println(a++);
+        // println (–2 * ( g – k ) +c);
+        // println (c=c++);
+        // println (c=++c*a++);
 
+        println("this are a java code (a,b,d,e). thearfor this ocde do not run becouse this is scala")
+        println("x++ and ++x and --x and x-- is do not work in scala")
+        println("println (- 2 * ( g - k ) +c); g is do not define variable and  '–' is not a '-' ")
+        
+        b = b - 1
+        println(b*a+c*d)
+        d = d - 1
+        
+        println(a)
+        
+        println(c)
+        
+        println( (c + 1)*a )
+        a = a + 1
+        
+        
+        
+
+
+    }
+}
     }
 }
